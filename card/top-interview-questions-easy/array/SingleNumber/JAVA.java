5a6d1c634158d82721befcfd82f41eca33c93580
tzzs <1789803837@qq.com>
2019-05-04 18:34:07 +0800

    card\top-interview-questions-easy\array\SingleNumber\JAVA.java

---
+++ card/top-interview-questions-easy/array/SingleNumber/JAVA.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int n : nums) {
+            res ^= n;
+        }
+        return res;
+    }
+}
+/**
+ * 使用异或处理
+ */
